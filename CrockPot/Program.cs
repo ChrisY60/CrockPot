5b83a461c8178debdbe8d181cef534b7ee29fad5
ChrisY60 <krisyankov@gmail.com>
2024-02-14 18:19:01 +0200

    Added azure vault to store the sendgrid API key.

--- CrockPot/Program.cs
+++ CrockPot/Program.cs
@@ -1,87 +1,91 @@
-    using CrockPot.Data;
-    using CrockPot.Services.IServices;
-    using CrockPot.Services;
-    using Microsoft.AspNetCore.Identity;
-    using Microsoft.EntityFrameworkCore;
-    using Azure.Storage.Blobs;
-    using Azure.Identity;
+using CrockPot.Data;
+using CrockPot.Services.IServices;
+using CrockPot.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Azure.Storage.Blobs;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Microsoft.Extensions.Configuration.AzureKeyVault;
+using System.Diagnostics;
 
-    var blobServiceClient = new BlobServiceClient(
-            new Uri("https://crockpotblob2005.blob.core.windows.net"),
-            new DefaultAzureCredential());
-
-
-    string containerName = "images";
-
-    BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
-
-    if (!await containerClient.ExistsAsync())
-    {
-        await containerClient.CreateAsync();
-    }
+var blobServiceClient = new BlobServiceClient(
+        new Uri("https://crockpotblob2005.blob.core.windows.net"),
+        new DefaultAzureCredential());
 
 
-    var builder = WebApplication.CreateBuilder(args);
+string containerName = "images";
 
-    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-    builder.Services.AddDbContext<ApplicationDbContext>(options =>
-        options.UseSqlServer(connectionString));
-    builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(containerName);
 
-    builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-        .AddRoles<IdentityRole>()
-        .AddEntityFrameworkStores<ApplicationDbContext>();
+if (!await containerClient.ExistsAsync())
+{
+    await containerClient.CreateAsync();
+}
 
 
-    builder.Services.AddScoped<ICategoryService, CategoryService>();
-    builder.Services.AddScoped<IIngredientService, IngredientService>();
-    builder.Services.AddScoped<IRecipeService, RecipeService>();
-    builder.Services.AddScoped<ICommentService, CommentService>();
-    builder.Services.AddScoped<IRatingService, RatingService>();
-    builder.Services.AddScoped<IBlobService, BlobService>();
-    builder.Services.AddScoped<ISharedRecipeService, SharedRecipeService>();
-    builder.Services.AddScoped<IEmailService, EmailService>();
-    builder.Services.AddControllersWithViews();
-
-    var app = builder.Build();
+var builder = WebApplication.CreateBuilder(args);
 
 
-    if (app.Environment.IsDevelopment())
-    {
-        app.UseDeveloperExceptionPage();
-        app.UseMigrationsEndPoint();
-    }
-    else
-    {
-        app.UseExceptionHandler("/Home/Error");
-        app.UseHsts();
-    }
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-    app.UseHttpsRedirection();
-    app.UseStaticFiles();
-
-    app.UseRouting();
-    app.UseStaticFiles();
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 
 
-    app.UseAuthentication();
-    app.UseAuthorization();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IIngredientService, IngredientService>();
+builder.Services.AddScoped<IRecipeService, RecipeService>();
+builder.Services.AddScoped<ICommentService, CommentService>();
+builder.Services.AddScoped<IRatingService, RatingService>();
+builder.Services.AddScoped<IBlobService, BlobService>();
+builder.Services.AddScoped<ISharedRecipeService, SharedRecipeService>();
+builder.Services.AddScoped<IEmailService, EmailService>();
+builder.Services.AddControllersWithViews();
 
-    app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}");
-    app.MapRazorPages();
+var app = builder.Build();
 
-    using (var scope = app.Services.CreateScope()){
-        var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
-        var roles = new[] { "Admin" };
 
-        foreach(var role in roles){
-            if(!await roleManager.RoleExistsAsync(role)) {
-                await roleManager.CreateAsync(new IdentityRole(role));
-            }
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseMigrationsEndPoint();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseStaticFiles();
+
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+using (var scope = app.Services.CreateScope()){
+    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    var roles = new[] { "Admin" };
+
+    foreach(var role in roles){
+        if(!await roleManager.RoleExistsAsync(role)) {
+            await roleManager.CreateAsync(new IdentityRole(role));
         }
     }
+}
 
 using (var scope = app.Services.CreateScope())
 {
--- CrockPot/Services/EmailService.cs
+++ CrockPot/Services/EmailService.cs
@@ -1,17 +1,31 @@
-﻿
-
-using CrockPot.Services.IServices;
+﻿using CrockPot.Services.IServices;
 using SendGrid;
 using SendGrid.Helpers.Mail;
-using System.Net.Mail;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 
 namespace CrockPot.Services
 {
     public class EmailService : IEmailService
     {
+        private readonly IConfiguration _configuration;
+
+        public EmailService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public async Task<bool> SendEmailAsync(string TargetAddress, string Subject, string HtmlContent)
         {
-            var apiKey = "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";
+            var keyVaultUrl = _configuration["KeyVault:KeyVaultUrl"];
+            var keyVaultClientId = _configuration["KeyVault:ClientId"];
+            var keyVaultClientSecret = _configuration["KeyVault:ClientSecret"];
+            var keyVaultDirectoryId = _configuration["KeyVault:DirectoryId"];
+
+            var credential = new ClientSecretCredential(keyVaultDirectoryId, keyVaultClientId, keyVaultClientSecret);
+            var secretClient = new SecretClient(new Uri(keyVaultUrl), credential);
+
+            var apiKey = (await secretClient.GetSecretAsync("SendGridAPIKey")).Value.Value;
             var client = new SendGridClient(apiKey);
             var from = new EmailAddress("crockpot0206@gmail.com", "CrockPot");
             var subject = Subject;
@@ -21,8 +35,6 @@ namespace CrockPot.Services
             var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
             var response = await client.SendEmailAsync(msg);
             return true;
-            //dobavi normalen return
         }
-
     }
 }
